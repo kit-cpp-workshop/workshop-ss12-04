92f21ff6fe55641ed48f7654f0b9d7c45903211b
Robert Schneider <Robert.Schneider3@student.kit.edu>
2012-05-24 23:11:18 +0200

    Points out uses of "-variant of include and ifdef

--- slides/cpp-code/conditional-inclusion.cpp
+++ slides/cpp-code/conditional-inclusion.cpp
@@ -1,13 +1,15 @@
+#ifdef WIN32
+#include <winsock2.h>
+#else
+#include <sys/net.h> // and more headers
+#endif
+
 #include <iostream>
 
 #define MY_ONE 1
 
 int main()
 {
-#if 2 == 1 + 1
-	std::cout << "2 is equal to 1 + 1";
-#endif
-
 #if 1 != MY_ONE
 	std::cout << "MY_ONE is NOT equal to 1";
 #else
