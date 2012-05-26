bf5a49a091a7fc55a859039d56648651b51459eb
Christian Buntin <christian.buntin@student.kit.edu>
2012-05-26 11:40:10 +0200

    Konstanten für die Figuren hinzugefügt

--- simple-chess/chess/general.h
+++ simple-chess/chess/general.h
@@ -13,4 +13,13 @@ const bool color_white = true;
 const bool color_black = false;
 
 const unsigned int chessboard_size = 8;
+
+const unsigned int id_none = 0;
+const unsigned int id_bauer = 1;
+const unsigned int id_springer = 2;
+const unsigned int id_laeufer = 3;
+const unsigned int id_turm = 4;
+const unsigned int id_dame = 5;
+const unsigned int id_koenig = 6;
+
 #endif // GENERAL_H_INCLUDED
