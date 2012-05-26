957ca98c614dd5bbcfbcd57a2b4c3d19cf713b90
Christian Buntin <christian.buntin@student.kit.edu>
2012-05-26 12:21:00 +0200

    UI-Funktionen zur Ausgabe und EIngabe hinzugefügt

--- simple-chess/chess/ui/userinterface.h
+++ simple-chess/chess/ui/userinterface.h
@@ -1,6 +1,9 @@
 #ifndef USERINTERFACE_H
 #define USERINTERFACE_H
 
+#include "../general.h"
+
+namespace chess {
 namespace ui {
 
 class UserInterface
@@ -8,9 +11,17 @@ class UserInterface
     public:
         UserInterface();
         virtual ~UserInterface();
-    protected:
-    private:
+        virtual void draw_board(chessboard board); /* Stellt das Schachbrett dar */
+
+        /* Holt sich die Koordinaten für den nächsten Zug des entsprechenden Spielers
+         in "from" und "to" */
+        virtual void read_move(bool player_color, coor &from, coor &to);
+
+        /* Ausgabe von Informationen */
+        virtual void print_error_message(int error_id);
+        virtual void print_info_message(int info_id);
 };
 
+}
 }
 #endif // USERINTERFACE_H
