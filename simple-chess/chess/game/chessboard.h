60184fb60f4b90003c3f15b5d440fc563cb59dba
Christian Buntin <christian.buntin@student.kit.edu>
2012-05-26 11:12:50 +0200

    Funktionen zur Abfrage des Bretts hinzugefügt

--- simple-chess/chess/game/chessboard.h
+++ simple-chess/chess/game/chessboard.h
@@ -7,7 +7,7 @@
 namespace game {
 
 /* Diese Klasse enthält das Spielbrett als Figuren-Array und dient zum durchführen der Züge.
-Da dies die einzige erlaubte manipulation ist, sind nicht mehr Befehle vorhanden.
+Da dies die einzige erlaubte Manipulation ist, sind nicht mehr Befehle dafür vorhanden.
 */
 class chessboard
 {
@@ -15,7 +15,12 @@ class chessboard
         chessboard();
         ~chessboard();
         // eigentlich könnte man auch Code von der piece-Klasse hierher übernehmen ...
+        // int wegen "Alles OK", "Ungültiger Zug" und "Gewonnen"
         int move(bool player_color, coord from, coord to);
+
+        // zur Darstellung:
+        int getID(coor field); //gibt ID zurück
+        bool getcolor(coor field);
     protected:
     private:
         piece[chessboard_size][chessboard_size];
