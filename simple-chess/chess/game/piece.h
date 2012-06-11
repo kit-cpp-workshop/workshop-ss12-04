ffdbe8ebf4c72cb2aa2a88add9ac3e7e13f19098
Christian Buntin <christian.buntin@student.kit.edu>
2012-06-11 20:04:15 +0200

    Figuren-Klassen erstellt

--- simple-chess/chess/game/piece.h
+++ simple-chess/chess/game/piece.h
@@ -10,27 +10,85 @@ class piece
 {
     public:
         /* Default-Konstruktor, für initialisierung des Spielfelds benötigt.
-        Daher ist diese Klasse auch nicht rein virtuell. */
+        Daher ist diese Klasse auch nicht rein virtuell.
+        Generell bezeichnet `piece` eine nicht vorhandene Figur, also ein leeres Feld. */
         piece();
-
-        piece(color player); // Figur mit Farbe erzeugen
 
         /* Überprüfung, ob ein Bewegen rein geometrisch von "from" nach "to"
         auf dem übergebenem Array möglich ist.
         Für jede Figur verschieden.
-        Ruft eventuell check_diagonal oder check_linear auf, um zu testen, ob was im Weg steht */
+        Ruft eventuell `check_line` auf, um zu testen, ob was im Weg steht */
         virtual bool possible_move(coor from, coor to, chessboard &spielfeld);
 
         color GetColour();
-
+        virtual chesspiece GetPiece();
     protected:
         color player; //Die Farbe der Figur
 
-        //Werden von "possible_move" von Dame, Läufer und Turm gebraucht:
+        //Werden von `possible_move` von Dame, Läufer und Turm gebraucht:
         bool check_line(coor from, coor to, chessboard &spielfeld);
 
 };
 
+namespace pieces {
+
+class none : public piece {
+    public:
+        none(color player);
+        bool possible_move(coor from, coor to, chessboard &spielfeld);
+        chesspiece GetPiece();
+};
+
+
+class pawn : public piece {
+    public:
+        pawn(color player);
+        bool possible_move(coor from, coor to, chessboard &spielfeld);
+        chesspiece GetPiece();
+};
+
+
+class rook : public piece {
+    public:
+        rook(color player);
+        bool possible_move(coor from, coor to, chessboard &spielfeld);
+        chesspiece GetPiece();
+};
+
+
+class knight : public piece {
+    public:
+        knight(color player);
+        bool possible_move(coor from, coor to, chessboard &spielfeld);
+        chesspiece GetPiece();
+};
+
+
+class bishop : public piece {
+    public:
+        bishop(color player);
+        bool possible_move(coor from, coor to, chessboard &spielfeld);
+        chesspiece GetPiece();
+};
+
+
+class queen : public piece {
+    public:
+        queen(color player);
+        bool possible_move(coor from, coor to, chessboard &spielfeld);
+        chesspiece GetPiece();
+};
+
+
+class king : public piece {
+    public:
+        king(color player);
+        bool possible_move(coor from, coor to, chessboard &spielfeld);
+        chesspiece GetPiece();
+};
+
+
+}
 }
 }
 #endif // PIECE_H
