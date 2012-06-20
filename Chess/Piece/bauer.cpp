ad3c1e67273b30e57aae6ed81d793cdd17916803
thomasspaeth <thomas-spaeth@web.de>
2012-06-20 18:23:29 +0200

    Zurueck zur alten Bauernimplementierung, neue funktioniert nicht

--- Chess/Piece/bauer.cpp
+++ Chess/Piece/bauer.cpp
@@ -19,6 +19,25 @@ bool Bauer::bewegungmoeglich(int a,int b,brett* brett)
 {
 	int deltay=std::abs(b-y);
 	int deltax=std::abs(a-x);
+
+	if (besitzer==1)
+	{
+	if ((b<y) && (deltax==0) && (deltay==1) && (brett->besitzer(a,b)==0)) { return true;}
+	if ((y==6) && (deltax==0) && (deltay==2) && (brett->besitzer(a,b-1)==0) && (brett->besitzer(a,b)==0)) { return true; }
+	if ((b<y) && (deltax==1) && (deltay==1) && (brett->besitzer(a,b)==2)) { return true; }
+	}
+	else
+	{
+	if ((b>y) && (deltax==0) && (deltay==1) && (brett->besitzer(a,b)==0)) { return true; }
+	if ((y==1) && (deltax==0) && (deltay==2) && (brett->besitzer(a,b-1)==0) && (brett->besitzer(a,b)==0)) { return true; }
+	if ((b>y) && (deltax==1) && (deltay==1) && (brett->besitzer(a,b)==1)) { return true; }
+	}
+
+return false;
+}
+	/*
+	int deltay=std::abs(b-y);
+	int deltax=std::abs(a-x);
 	int yZugrichtung = (besitzer == 1) ? (-1) : (1);
 	int anderer = (besitzer ==1) ? (2) : (1);
 
@@ -37,7 +56,7 @@ bool Bauer::bewegungmoeglich(int a,int b,brett* brett)
 	}
 
 	return false;
-}
+}*/
 
 Bauer::Bauer(int a,int b,int c):Piece(a,b,c)
 {
